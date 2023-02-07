0314b0cbd9dbdee6ac6811bebafaa8e4cdf0d163
Fondao Sriworasa <fondao21011999@gmail.com>
2023-02-07 12:18:13 +0700

    update to-do list

---
+++ todo-list/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Navber</h1>
+    </div>
+  );
+}
+
+export default Navbar;
